c6cd350d19b1995cedbf8c1fbaafaf7a86965937
Haider Al-Barakat <123415625+Haider037@users.noreply.github.com>
2024-11-10 23:52:14 -0600

    Update frontend/src/app/(auth)/signup/SignUpForm.tsx
    Co-authored-by: Jose Chacon <124737162+JoseChaconUrias@users.noreply.github.com>

--- frontend/src/app/(auth)/signup/SignUpForm.tsx
+++ frontend/src/app/(auth)/signup/SignUpForm.tsx
@@ -7,7 +7,7 @@ const SignUpForm = () => {
         <h2 className='text-xl font-bold mb-10'>Sign Up Start Shopping</h2>
         <div className='flex flex-col items-center'>
             <input className='outline p-2 rounded-xl w-full mb-3'placeholder='Full Name' type="text" required/>
-            <input className='outline p-2 rounded-xl w-full mb-3' placeholder='Email Address' type="text" required/>
+            <input className='outline p-2 rounded-xl w-full mb-3' placeholder='Email Address' type="email" required/>
             <input className='outline p-2 rounded-xl w-full mb-3'placeholder='Username' type ="text" required/>
             <input className='outline p-2 rounded-xl w-full mb-3'placeholder='Password' type='password' required/>
             <input className='outline p-2 rounded-xl w-full mb-5' placeholder='Confirm Password' type="password" required/>
