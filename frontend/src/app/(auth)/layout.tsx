d6fc1fefb48cbaaeb135b6e92c81c01292c66868
Jose Chacon <jcurias123@gmail.com>
2024-11-11 11:11:05 -0600

    Add aceternityui Flip Words animation

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
+import { FlipWords } from "@/components/ui/FlipWords";
+
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-row">
       <section className="flex w-1/2">
         {children}
       </section>
-      <div className="w-1/2">
-        <p>*photo goes here*</p>
+      <div className="flex flex-col justify-center w-1/2 text-6xl">
+        <h1>Shop <FlipWords words={["easier", "faster", "seemlessly"]} /></h1>
+        <h1>with our online Grocery Store</h1>
       </div>
     </main>
   );
