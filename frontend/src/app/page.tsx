7a1432ac668a3f9ac0ae9f563ce317a7350c829c
Jose Chacon <jcurias123@gmail.com>
2024-11-15 16:35:10 -0600

    Fix rendered padding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 import Navbar from "./navbar/Navbar";
 import { ProductCards } from "@/components/products/ProductCards";
 
-
 const Home = () => {
   return (
-    <main className="min-h-screen p-20">
+    <main className="min-h-screen">
       <Navbar />
-      <div className="flex flex-col gap-10">
-        <ProductCards title={"Produce"}/>
-        <ProductCards title={"Meat"}/>        
-      </div>
+      <section className="p-20">
+        <div className="flex flex-col gap-10">
+          <ProductCards title={"Produce"} />
+          <ProductCards title={"Meat"} />
+        </div>        
+      </section>
+
     </main>
   );
 };
