15daaa504ef3dd9b61798e2002c087abc11a15ce
morenzorafael <morenzo.rafael@gmail.com>
2024-12-08 17:49:20 -0600

    Delete backend/Product/IProductAccessor.cs

--- backend/Product/IProductAccessor.cs
+++
@@ -1,7 +0,0 @@
-public interface IProductAccessor 
-{
-  public Product GetProductById(int productId);
-  public List<Product> GetAllProducts();
-  public void UpdateProductStock(int productId, int stock);
-  public void ApplyDiscountToProduct(int productId, double discount);
-}
