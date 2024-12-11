7523312c564eee97d982835f7a3afcd4d0219940
Haider037 <hal-barakat@unomaha.edu>
2024-12-10 21:22:03 -0600

    Rebrand profile, cart, and checkout pages.

--- frontend/src/app/cart/Cart.tsx
+++ frontend/src/app/cart/Cart.tsx
@@ -1,12 +1,18 @@
+"use client"
 import { FlipWords } from "@/components/ui/FlipWords";
 import Link from "next/link";
+import { useState } from "react";
 
 const CartPage = () => {
+  const[itemCount, setItemCount] = useState<string>("5");
+  const [amountDue, setAmountDue] = useState<string>("$15.27");
+  const [shoppingCart, setShoppingCart] = useState<any>([]);
+
   return (
-    <div className="bg-white flex felx col items-center w-[30rem] rounded shadow-md">
+    <div className="bg-zinc-950 text-zinc-100 flex felx col items-center w-[30rem] rounded shadow-md">
       <div className="flex flex-col items-center w-full">
         <h1 className="absolute left-28">
-          <Link href="/" className="text-blue-500 font-bold">
+          <Link href="/" className="text-purple-500 font-bold">
             Back to Shopping
           </Link>
         </h1>
@@ -14,22 +20,22 @@ const CartPage = () => {
           Cart it
           <FlipWords words={["Quick", "Fresh", "Clean"]} />
         </h1>
-        <div className=" flex flex-col items-center mr-12">
-          <div className="mr-12 mb-80">
-            <span className="font-bold mr-5">Shopping Cart: </span>
+        <div className=" flex flex-col mr-12">
+          <div className="mb-80">
+            <span className="font-bold">Shopping Cart: </span>
             <span className="ml-2"></span>
           </div>
-          <div className="mr-12">
-            <span className="font-bold mr-12 mb-3">Item Count: </span>
-            <span className="ml-2"></span>
+          <div className="">
+            <span className="font-bold mb-3">Item Count: {itemCount} </span>
+            <span className=""></span>
           </div>
-          <div className="mb-4 mr-2">
-            <span className="font-bold mr-20">Amount Due:</span>
-            <span className="ml-2"></span>
+          <div className="mb-3">
+            <span className="font-bold">Amount Due: {amountDue}</span>
+            <span className=""></span>
           </div>
           <div>
             <a href="/checkout">
-              <button className="rounded-3xl font-bold bg-emerald-400 w-32 p-2 mb-6">
+              <button className="ml-6 rounded-3xl font-bold bg-purple-500 w-32 p-2 mb-6">
                 Checkout
               </button>
             </a>
