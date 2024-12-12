ff2e704585b6f4cfdedb92c8520e3cf7a41a44e5
dparks6 <111796886+dparks6@users.noreply.github.com>
2024-12-11 21:17:25 -0600

    Delete current Cart directory to make room for new format
    Removing the current Cart class to make room to add in the new Cart class with the updated format

--- backend/Cart/Cart.cs
+++
@@ -1,7 +0,0 @@
-public class Cart
-{
-    public int cartId { get; set; }
-    public string userId { get; set;
-    public SortedDictionary<Product, int> itemList { get; set;}
-    public double totalPrice { get; set; }
-}
--- backend/Cart/ICartAccessor.cs
+++
@@ -1,9 +0,0 @@
-public interface ICartAccessor
-{
-    public Cart getUserCart(string userId);
-    void addToCart(int cartId, Product product, int amount);
-    void removeFromCart(int cartId, Product product);
-    void updateAmount(int cartId, Product product, int amount);
-    void initiateCart(Cart cart);
-    void clearCart(int cartId);
-}
