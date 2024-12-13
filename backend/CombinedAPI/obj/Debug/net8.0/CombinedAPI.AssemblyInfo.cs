f19c88407b20936c892bd47c86f7572f9d0b84c3
Jose Chacon <jcurias123@gmail.com>
2024-12-13 13:51:48 -0600

    Delete unused interfaces

--- backend/CombinedAPI/obj/Debug/net8.0/CombinedAPI.AssemblyInfo.cs
+++ backend/CombinedAPI/obj/Debug/net8.0/CombinedAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8659e90a6cd40b6738f75443b2a8e548e685a471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3ee49f9c7022378c03098f381db5b0437f317f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
