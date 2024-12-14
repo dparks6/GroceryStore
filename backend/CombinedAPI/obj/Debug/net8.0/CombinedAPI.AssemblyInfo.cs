aa4024eb0525cddd9e4cce7f39a9dc5f2d71c2ce
Jose Chacon <jcurias123@gmail.com>
2024-12-13 23:08:50 -0600

    Added fetch to parent node

--- backend/CombinedAPI/obj/Debug/net8.0/CombinedAPI.AssemblyInfo.cs
+++ backend/CombinedAPI/obj/Debug/net8.0/CombinedAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3ee49f9c7022378c03098f381db5b0437f317f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d76ce185f29af57a6f35e9c20c27e14be6de9c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombinedAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/products/ProductCards.tsx
+++ frontend/src/components/products/ProductCards.tsx
@@ -1,12 +1,33 @@
 import Cards from "./Cards";
 
-const ProductCards = () => {
+const fetchProductCategoryById = async (id : number) => {
+  const response = await fetch(`http://localhost:5058/api/product/category/${id}`);
+  return response.json();
+}
+
+const ProductCards = async () => {
+  const responseFreshProduce = await fetchProductCategoryById(1);
+  const responseDairy = await fetchProductCategoryById(2);
+  const responseDeli = await fetchProductCategoryById(3);
+
+  const sections = [
+    { title: "Fresh Produce", products: responseFreshProduce, id: 1 },
+    { title: "Dairy", products: responseDairy, id: 2 },
+    { title: "Deli", products: responseDeli, id: 2 },
+  ];
+
   return (
     <>
-      <h1 className="text-4xl font-bold mb-10">Fresh Produce</h1>
-      <div className="flex flex-row p-4 gap-10 overflow-x-scroll overscroll-x-auto scroll-smooth [scrollbar-width:none]">
-        <Cards />
-      </div>
+      {sections.map((section) => (
+        <div key={section.title}>
+          <h1 className="text-4xl font-bold mb-10">{section.title}</h1>
+          <div className="flex flex-row p-4 gap-10 overflow-x-scroll overscroll-x-auto scroll-smooth [scrollbar-width:none]">
+            <Cards categoryId={section.id} />
+          </div>               
+        </div>
+   
+      ))}
+
     </>
   );
 };
