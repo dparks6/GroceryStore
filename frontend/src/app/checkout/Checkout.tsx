bbb0f02f9ce4623a072c4a24cbe937461f828aaa
Haider037 <hal-barakat@unomaha.edu>
2024-12-14 18:48:48 -0600

    Fix issue in productCards.tsx. Display user information in profile and checkout page.

--- frontend/src/app/checkout/Checkout.tsx
+++ frontend/src/app/checkout/Checkout.tsx
@@ -1,13 +1,12 @@
-"use client"
 import { FlipWords } from "@/components/ui/FlipWords";
 import Link from "next/link";
 import React, { useState } from "react";
+import { getUserInfo } from "../profile/ProfilePage";
 
-const CheckoutPage = () => {
-  const[fullName, setFullName] = useState<string>("John Doe");
-  const [email, setEmail] = useState<string>("john.doe@fakemail.com");
-  const [number, setNumber] = useState<string>("123-456-7890)")
-  const [address, setAddress] = useState<string>("1234 Sesame St");
+const CheckoutPage = async () => {
+  var id = 1;
+  const userInfo = await getUserInfo(1);
+
   return (
     <form className="bg-zinc-950 text-zinc-100  p-5 w-[30rem] h-full flex flex-col rounded-xl">
       <div className="flex flex-col items-center w-full">
@@ -24,26 +23,24 @@ const CheckoutPage = () => {
       <div className="flex flex-col ml-6 ">
         <div className="">
           <span className="font-bold">Full Name: </span>
-          <span className="">{fullName}</span>
+          <span className="">
+            {userInfo.firstName} {userInfo.lastName}
+          </span>
         </div>
         <div>
           <span className="font-bold">Email Address: </span>
-          <span className="">{email}</span>
+          <span className="">{userInfo.email}</span>
         </div>
         <div className="">
           <span className="font-bold">Phone Number: </span>
-          <span className="">{number}</span>
+          <span className="">{userInfo.phoneNumber}</span>
         </div>
         <div className=" mb-5">
           <span className="font-bold">Address: </span>
-          <span className="">{address}</span>
+          <span className="">{userInfo.address}</span>
         </div>
       </div>
       <div className=" flex flex-col">
-        <div className=" mb-40">
-          <span className="font-bold ml-6">Shopping Cart: </span>
-          <span className=""></span>
-        </div>
         <div className="">
           <span className="font-bold mb-3 ml-6">Item Count: </span>
           <span className=""></span>
--- frontend/src/app/profile/ProfilePage.tsx
+++ frontend/src/app/profile/ProfilePage.tsx
@@ -1,44 +1,71 @@
-"use client"
-import { FlipWords } from '@/components/ui/FlipWords'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { FlipWords } from "@/components/ui/FlipWords";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const ProfilePage = () => {
-  const router = useRouter();
-  const[fullName, setFullName] = useState<string>("John Doe");
-  const [email, setEmail] = useState<string>("john.doe@fakemail.com");
-  const [username, setUsername] = useState<string>("doe123");
-  const [address, setAddress] = useState<string>("1234 Sesame St");
-  const [dateJoined, setDateJoined] = useState<string>("12/01/2024");
-  const[number, setNumber] = useState<string>("123-456-7890");
-  const [error, setError] = useState<string | null>(null);
-
-  return (
-    <div className='bg-zinc-950 text-zinc-100 p-5 w-[30rem] h-full flex flex-col rounded-xl'>
-      <div className='flex flex-col items-center w-full'>
-      <h1 className='absolute left-28'><Link href="/" className="text-purple-500 font-bold">Back to Shopping</Link></h1>
-        <h1 className='mt-10 text-2xl font-bold mb-2'>Welcome<FlipWords words={["Loyal", "Valued", "Home" ]}/>Customer</h1>
-      </div>
-        <div className=' flex flex-col text-left px-6'>
-          <div className=''>
-          <span className='font-bold'>Full Name:</span><span className='ml-2'>{fullName}</span>
-          </div>
-          <div className=''>
-          <span className='font-bold'>Date Joined:</span><span className='ml-2'>{dateJoined}</span>
-          </div>
-          <div>
-          <span className='font-bold'>Email Address:</span><span className='ml-2'>{email}</span>
-          </div>
-          <div className=''>
-          <span className='font-bold'>Phone Number:</span><span className='ml-2'>{number}</span>
-          </div>
-          <div className=''>
-          <span className='font-bold'>Address:</span><span className='ml-2'>{address}</span>
-          </div>
-        </div>
-    </div>
-  )
+export interface User {
+  userID: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  creditcardNumber: string;
+  creditcardExpDate: string;
+  cvv: number;
+  shippingLocation: string;
 }
 
-export default ProfilePage
+export const getUserInfo = async (id: number): Promise<User> => {
+  const response = await fetch(`http://localhost:5058/api/user/id/${id}`);
+  const userInfo: User = await response.json();
+
+  return userInfo;
+};
+const ProfilePage = async () => {
+  var id = 1;
+  const userInfo = await getUserInfo(1);
+  return (
+    <div className="bg-zinc-950 text-zinc-100 p-5 w-[30rem] h-full flex flex-col rounded-xl">
+      <div className="flex flex-col items-center w-full">
+        <h1 className="absolute left-28">
+          <Link href="/" className="text-purple-500 font-bold">
+            Back to Shopping
+          </Link>
+        </h1>
+        <h1 className="mt-10 text-2xl font-bold mb-2">
+          Welcome
+          <FlipWords words={["Loyal", "Valued", "Home"]} />
+          Customer
+        </h1>
+      </div>
+      <div className=" flex flex-col text-left px-6">
+        <div className="">
+          <span className="font-bold">Full Name:</span>
+          <span className="ml-2">
+            {userInfo.firstName} {userInfo.lastName}
+          </span>
+        </div>
+        <div>
+          <span className="font-bold">Email Address:</span>
+          <span className="ml-2">{userInfo.email}</span>
+        </div>
+        <div>
+          <span className="font-bold">Username:</span>
+          <span className="ml-2">{userInfo.username}</span>
+        </div>
+        <div className="">
+          <span className="font-bold">Phone Number:</span>
+          <span className="ml-2">{userInfo.phoneNumber}</span>
+        </div>
+        <div className="">
+          <span className="font-bold">Address:</span>
+          <span className="ml-2">{userInfo.address}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
